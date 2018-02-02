f460bbf7fed6a82d01109ac085737e3a4379d46d
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-02 08:40:35 +0530

    ex10 lab test fixed

--- test/excercise10.js
+++ test/excercise10.js
@@ -1,25 +1,24 @@
-const supertest = require('supertest');
-
 const { expect } = require('code');
 const labScript = require('lab').script();
 
 const { it } = labScript;
 exports.lab = labScript;
 
-const server = require('../src/excercise8');
+const server = require('../src/excercise10');
 
-it('Ideal case', (done) => {
+it('Not guest, username but not accesstoken', (done) => {
   const jsonObj = {
     isGuest: false,
     username: 'a',
     accessToken: 'a1',
-    password: 'b2',
   };
-  supertest(server.listener)
-    .post('/login', jsonObj)
-    .then((response) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    })
-    .catch(console.log);
+  const req = {
+    method: 'POST',
+    url: '127.0.0.1:8080/login',
+    payload: JSON.stringify(jsonObj),
+  };
+  server.inject(req, (res) => {
+    expect(res.statusCode).to.equal(200);
+    done();
+  });
 });
