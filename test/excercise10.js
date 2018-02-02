21b86a78eb1247f2ea34cac71012e16c7438e218
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-02 08:44:03 +0530

    not guest tests passed

--- test/excercise10.js
+++ test/excercise10.js
@@ -6,11 +6,80 @@ exports.lab = labScript;
 
 const server = require('../src/excercise10');
 
-it('Not guest, username but not accesstoken', (done) => {
+it('Not guest, username but not password', (done) => {
   const jsonObj = {
     isGuest: false,
     username: 'a',
     accessToken: 'a1',
+  };
+  const req = {
+    method: 'POST',
+    url: '127.0.0.1:8080/login',
+    payload: JSON.stringify(jsonObj),
+  };
+  server.inject(req, (res) => {
+    expect(res.statusCode).to.equal(200);
+    done();
+  });
+});
+
+it('Not guest, username, password and accesstoken', (done) => {
+  const jsonObj = {
+    isGuest: false,
+    username: 'a',
+    accessToken: 'a1',
+    password: 'b1',
+  };
+  const req = {
+    method: 'POST',
+    url: '127.0.0.1:8080/login',
+    payload: JSON.stringify(jsonObj),
+  };
+  server.inject(req, (res) => {
+    expect(res.statusCode).to.equal(400);
+    done();
+  });
+});
+
+it('Not guest, invalid username', (done) => {
+  const jsonObj = {
+    isGuest: false,
+    username: 1234,
+    accessToken: 'a1'
+  };
+  const req = {
+    method: 'POST',
+    url: '127.0.0.1:8080/login',
+    payload: JSON.stringify(jsonObj),
+  };
+  server.inject(req, (res) => {
+    expect(res.statusCode).to.equal(400);
+    done();
+  });
+});
+
+it('Not guest, invalid accesstoken', (done) => {
+  const jsonObj = {
+    isGuest: false,
+    username: 'a',
+    accessToken: 69,
+  };
+  const req = {
+    method: 'POST',
+    url: '127.0.0.1:8080/login',
+    payload: JSON.stringify(jsonObj),
+  };
+  server.inject(req, (res) => {
+    expect(res.statusCode).to.equal(400);
+    done();
+  });
+});
+
+it('Not guest, username but not accesstoken', (done) => {
+  const jsonObj = {
+    isGuest: false,
+    username: 'a',
+    password: 'a1',
   };
   const req = {
     method: 'POST',
